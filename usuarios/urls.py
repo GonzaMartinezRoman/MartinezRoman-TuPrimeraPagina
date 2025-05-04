1ba1555b774f7059b57a2a0536eae7fd44c78a9b
GonzaMartinezRoman <martinezromang@gmail.com>
2025-05-04 17:28:54 -0300

    Agrego el logout y le doy el formato y mesaje de cierre de sesión

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from usuarios.views import login_usuarios
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', login_usuarios, name="login"),
-    
+    path('logout/', LogoutView.as_view(template_name='usuarios/logout.html'), name="logout"),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def login_usuarios(request):
-    return render(request, 'usuarios/login.html')
+    if request.method == 'POST':
+        formulario = AuthenticationForm(request, data=request.POST)
+        if formulario.is_valid():
+            
+            usuario = formulario.get_user()
+
+            login(request, usuario)
+            
+            return redirect('inicio')
+    else:
+        formulario = AuthenticationForm()
+
+    return render(request, 'usuarios/login.html', {'formulario':formulario})
